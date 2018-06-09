f81e6c425a595393c32cc7b136e84c0c8f1429b1
Osama Aamer <osama.aamer95@gmail.com>
2018-06-09 20:52:57 +0500

    Create quiz core component to glue together the rest

---
+++ src/components/QuizCore.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import quiz components
+import Word from '../components/Word';
+import Progress from '../components/Progress';
+import Answers from '../components/Answers';
+
+function QuizCore(props) {
+    function generateAnswers(key) {
+        return (
+            <Answers
+            key={key.meaning}
+            answerContent={key.meaning}                                      
+            />
+        );
+    }
+
+    return (
+       <div className="quiz">
+       {/* quiz progress */}
+         <Progress
+           counter={props.questionId}
+           total={props.questionTotal}
+         />
+         {/* quiz question */}
+         <Word content={props.question} />
+         {/* generates answer options using map function */}
+         <ul className="answerOptions">
+           
+         </ul>
+       </div>
+    );
+  }
+
+  QuizCore.propTypes = {
+    answer: PropTypes.string.isRequired,
+    answerOptions: PropTypes.array.isRequired,
+    counter: PropTypes.number.isRequired,
+    question: PropTypes.string.isRequired,
+    questionId: PropTypes.number.isRequired,
+    questionTotal: PropTypes.number.isRequired
+  };
+
+  export default QuizCore;
